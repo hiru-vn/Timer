47025e759050c4b8feb390c39a51fc3dd9d33943
quanghuyUIT <quanghuy1998kh@gmail.com>
2019-08-22 14:57:17 +0700

    rebulid

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.timer
+package com.example.myapplication
 
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.timer", appContext.packageName)
+        assertEquals("com.example.myapplication", appContext.packageName)
     }
 }
--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.timer
+package com.example.myapplication
 
 import org.junit.Test
 
